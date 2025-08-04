38095ee42db468e0d1887a373c5a56fc229688fa
AnmolDevero <anmoldevero974@gmail.com>
2025-08-04 19:17:46 +0500

    Add password change templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('signup/', views.signup_view, name= 'signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password/change', auth_views.PasswordChangeView.as_view(template_name='change_password.html'), name='password_change'),
+    path('password/change/done', auth_views.PasswordChangeDoneView.as_view(template_name='change_password_done.html'), name='passwrod_change_done'),
     path('delete_acount/',  views.delete_acount, name='delete_acount')
 ]
 
